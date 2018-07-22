5a26e28cf1a05b1ae76dba24e1458124dec94254
Ondřej Ježil <firma.ondra@gmail.com>
2018-07-22 12:06:49 +0200

    Test commit

---
+++ src/main.c
@@ -0,0 +1,69 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "locale.h"
+#include "wchar.h"
+
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#ifndef _O_U8TEXT
+  #define _O_U8TEXT 0x00040000
+#endif
+#endif
+
+unsigned int value;
+
+typedef struct command{
+	wchar_t * name;
+	void(*funct)(unsigned int);
+} command;
+
+command* create_command(wchar_t *str, void(*sfun)(unsigned int))
+{
+	command *cmd = malloc(sizeof(command));
+	cmd->name = str;
+	cmd->funct = sfun;
+	return cmd;
+}
+
+void wrut(unsigned int p)
+{
+	wprintf(L"%lc",(wchar_t)value);
+}
+
+void wred(unsigned int p)
+{
+	 wchar_t emj;
+	 wscanf(L"%c",&emj);
+	 wprintf(L"%lc",emj);
+	 wprintf(L"%lc",(wchar_t)'✒');
+	 value = emj;
+}
+
+void chad(unsigned int p)
+{
+	 value = p;
+}
+
+int main()
+{
+	setlocale(LC_ALL,"cs_CZ.UTF-8");
+	#ifdef _WIN32
+		_setmode(_fileno(stdout), _O_U8TEXT);
+	#endif
+
+	command* commands[] = {create_command("write",&wrut),
+			create_command("read",&wred),
+			create_command("set",&chad)};
+
+
+	//commands[2]->funct((int)'✒');
+	//commands[0]->funct(0);
+
+	wprintf(L"%lc",(wchar_t)L'✒');
+
+	return 0;
+}
+
+
+//😂   💑  ✏  ✒
